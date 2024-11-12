bacc02ee178b218248af71970068c58f8b3bc605
dominickdevincenzo <devindm101@gmail.com>
2024-11-12 11:35:48 -0500

    reworked server.js to mimic HW3, created routes.js file

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+const config = require("./config");
+const routes = require("./routes");
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-// Middleware for parsing JSON
-app.use(express.json());
-
-// Basic route for testing
+// Route for testing
 app.get("/", (req, res) => {
   res.send("Express server running!");
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(config.server_port, () => {
+  console.log(
+    `Server running at http://${config.server_host}:${config.server_port}/`
+  );
 });
+
+module.exports = app;
