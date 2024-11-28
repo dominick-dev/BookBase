d30cf87f2fe9bdb4c70be50453f1a29eef437f49
dominickdevincenzo <devindm101@gmail.com>
2024-11-28 00:00:28 -0500

    got user loging auth working with db

--- server/server.js
+++ server/server.js
@@ -2,20 +2,96 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./config");
 const routes = require("./routes");
-// const mongoose = require("mongoose");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const authRoutes = require("./auth");
+const User = require("./models/User");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 
-// const mongoUri = config.mongo_uri;;
-
-// mongoose.connect(mongoUri)
-//   .then(() => console.log("MongoDB connected successfully"))
-//   .catch(err => console.log('MongoDB connection error: ', err));
-
+// MongoDB connection
+const mongoUri = config.mongo_uri;
+mongoose.connect(mongoUri)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.log('MongoDB connection error: ', err));
+  
 const app = express();
+
+// Middleware
 app.use(
   cors({
     origin: "*",
   })
 );
+
+app.use(express.json());
+
+app.use(passport.initialize());
+
+
+// google OAuth Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.google_client_id,
+      clientSecret: config.google_client_secret,
+      callbackURL: config.google_callback_url,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const email = profile.emails[0].value;
+
+      try {
+        let user = await User.findOne({ email });
+        if (!user) {
+          user = new User({
+            email,
+            provider: 'google',
+            providerAccountId: profile.id,
+            user_id: new mongoose.Types.ObjectId(),
+          });
+          await user.save();
+        }
+        done(null, user);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+)
+
+// facebook OAuth Strategy
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: config.facebook_client_id,
+      clientSecret: config.facebook_client_secret,
+      callbackURL: config.facebook_redirect_uri,
+      profileFields: ['id', 'emails', 'name'],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const email = profile.emails[0].value;
+
+      try {
+        let user = await User.findOne({ email });
+        if (!user) {
+          user = new User({
+            email,
+            provider: 'facebook',
+            providerAccountId: profile.id,
+            user_id: new mongoose.Types.ObjectId(),
+          });
+          await user.save();
+        }
+        done(null, user);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+)
+
+// routes
+app.use("/auth", authRoutes);
 
 // Route to test that server is running
 app.get("/", (req, res) => {
