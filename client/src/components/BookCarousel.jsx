7d7f3879d07bfef29fdfd9f237fd58433fe58bd9
zloudini <150490198+zloudini@users.noreply.github.com>
2024-11-30 13:59:03 -0500

    added BookCarousel, install Swiper dependency

--- client/src/components/BookCarousel.jsx
+++ client/src/components/BookCarousel.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
+//import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BookCard from './BookCard';
+
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import { EffectCoverflow, Navigation, Mousewheel} from 'swiper/modules'
 
 const BookCarousel = () => {
   const [books, setBooks] = useState([]);
@@ -31,7 +40,99 @@ const BookCarousel = () => {
 
   return (
     <>
-    <div className="container mt-5" style={{padding: '0'}}>
+    <div className="carousel" style={{backgroundColor: '#333', paddingTop: '12px', paddingBottom: '12px'}}>
+    <Swiper
+        direction="horizontal"
+        effect={'coverflow'}
+        loop={true}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'4'}
+        mousewheel={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 50,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Mousewheel, Navigation]}
+        className="mySwiper"
+    >
+        {books.map((book, index) => (
+            book ? (
+                <SwiperSlide key={index}>
+                <BookCard book={book}/>
+                </SwiperSlide>
+        ) : null
+    ))}
+    </Swiper>
+    </div>
+    
+    
+    {/* <Carousel
+    additionalTransfrom={0}
+    arrows
+    autoPlaySpeed={3000}
+    centerMode
+    className=""
+    containerClass="container"
+    dotListClass=""
+    draggable
+    focusOnSelect={false}
+    infinite
+    itemClass=""
+    keyBoardControl
+    minimumTouchDrag={80}
+    pauseOnHover
+    renderArrowsWhenDisabled={false}
+    renderButtonGroupOutside={false}
+    renderDotsOutside={false}
+    responsive={{
+        desktop: {
+        breakpoint: {
+            max: 3000,
+            min: 1024
+        },
+        items: 3,
+        partialVisibilityGutter: 40
+        },
+        mobile: {
+        breakpoint: {
+            max: 464,
+            min: 0
+        },
+        items: 1,
+        partialVisibilityGutter: 30
+        },
+        tablet: {
+        breakpoint: {
+            max: 1024,
+            min: 464
+        },
+        items: 2,
+        partialVisibilityGutter: 30
+        }
+    }}
+    rewind={false}
+    rewindWithAnimation={false}
+    rtl={false}
+    shouldResetAutoplay
+    showDots={false}
+    sliderClass=""
+    slidesToSlide={1}
+    swipeable
+    >
+        {books.map((book, index) => (
+            book ? (
+                <BookCard book={book}/>
+        ) : null
+    ))}
+    </Carousel> */}
+
+
+    {/* <div className="container mt-5" style={{padding: '0'}}>
         <h1 className="text-center mb-4">Book Carousel</h1>
         <Carousel style={{width:'100%'}}>
             {books.map((book, index) => (
@@ -42,7 +143,7 @@ const BookCarousel = () => {
             ) : null // Do not render if book is invalid
             ))}
         </Carousel>
-    </div>
+    </div> */}
     </>
   );
 };
