cc9cce92a5eb914373ee481bc5c3a620c714ac18
Aadit Vyas <aaditv@seas.upenn.edu>
2024-12-01 21:52:37 -0500

    Simplify the navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -44,45 +44,12 @@ function NavBar() {
           {/* Logo */}
           <Logo />
 
-          {/* Navigation Menu for Small Screens */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end' }}>
-            <IconButton
-              size="large"
-              aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-            >
-              <NavButton to="/search" label="Search" isMobile={true} onClick={handleCloseNavMenu} />
-              <NavButton to="/insights" label="Interesting Insights" isMobile={true} onClick={handleCloseNavMenu} />
-            </Menu>
-          </Box>
-
-          {/* Desktop Navigation Buttons */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+          {/* Unified Navigation Buttons for All Screens */}
+          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
             <NavButton to="/insights" label="Interesting Insights" isMobile={false} />
+            <NavButton to="/search" label="Search For Books" isMobile={false} />
           </Box>
 
-          {/* Search Component */}
-          <SearchComponent_nav />
           {/* Account Button */}
           <AccountMenu
             anchorElUser={anchorElUser}
