44845afe8c543ff512a0bbb9effe63ee58dd9c3f
dominickdevincenzo <devindm101@gmail.com>
2024-12-02 15:54:40 -0500

    navagate to /profile on Profile btn click in AccountMenue.jsx

--- client/src/components/AccountMenu.jsx
+++ client/src/components/AccountMenu.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import { Box, Tooltip, IconButton, Avatar, Menu, MenuItem, Typography } from '@mui/material';
+import React from "react";
+import {
+  Box,
+  Tooltip,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 
-const AccountMenu = ({ anchorElUser, handleOpenUserMenu, handleCloseUserMenu }) => {
+const AccountMenu = ({
+  anchorElUser,
+  handleOpenUserMenu,
+  handleCloseUserMenu,
+}) => {
   return (
-    <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center' }}>
+    <Box sx={{ flexGrow: 0, display: "flex", alignItems: "center" }}>
       <Tooltip title="Open settings">
         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
         </IconButton>
       </Tooltip>
       <Menu
-        sx={{ mt: '45px' }}
+        sx={{ mt: "45px" }}
         id="menu-appbar"
         anchorEl={anchorElUser}
         anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
+          vertical: "top",
+          horizontal: "right",
         }}
         keepMounted
         transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
+          vertical: "top",
+          horizontal: "right",
         }}
         open={Boolean(anchorElUser)}
         onClose={handleCloseUserMenu}
       >
-        {['Profile', 'Account', 'Dashboard', 'Logout'].map((setting) => (
+        {["Profile", "Account", "Dashboard", "Logout"].map((setting) => (
           <MenuItem key={setting} onClick={handleCloseUserMenu}>
             <Typography textAlign="center">{setting}</Typography>
           </MenuItem>
