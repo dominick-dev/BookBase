a99717201723a8331f5e8d87d9785cdd67642eba
hi-jaxon <jaxonbailey01@gmail.com>
2024-12-02 18:04:31 -0500

    added navbar and changed background

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -3,13 +3,15 @@ import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
 import { Container, Dropdown, DropdownButton } from "react-bootstrap";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import NavBar from "../components/NavBar";
+import '../styles/MapPage.css';
 
 
 const MapPage = () => {
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState("");
   const [markers, setMarkers] = useState([]);
-  const [zoomLevel, setZoomLevel] = useState(1);
+  const [zoomLevel, setZoomLevel] = useState(2);
 
 
   useEffect(() => {
@@ -38,7 +40,7 @@ const MapPage = () => {
       // search database based on country name
       const reviewResponse = await fetch(`http://localhost:8080/reviewsWithCoordinates/${countryName}`);
       const reviewData = await reviewResponse.json();
-      setZoomLevel(1);
+      setZoomLevel(2);
 
       if (reviewData.length > 0) {
 
@@ -61,59 +63,73 @@ const MapPage = () => {
       console.error("Error fetching markers:", error);
     }
 
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    width: '100%',
+  }
 
   };
 
   return (
-    <Container>
-      <DropdownButton
-        id="country-dropdown"
-        title={selectedCountry || "Select country"}
-        onSelect={handleSelect}
-        className="my-5"
-      >
-        <div style={dropdownMenuStyle}>
-          {countries && countries.length > 0 ? (  // if countries retrieved
-            countries.map((item, index) => (  // display them in dropdpwn
-              <Dropdown.Item key={index} eventKey={item.country}>
-                {item.country}
-              </Dropdown.Item>
-            ))
-          ) : (
-            <Dropdown.Item>Loading countries...</Dropdown.Item>
-          )}
-        </div>
-      </DropdownButton>
+    <>
+    <NavBar/>
+      <div className="map-page-body">
+        <Container>
+          <DropdownButton
+            id="country-dropdown"
+            title={selectedCountry || "Select country"}
+            onSelect={handleSelect}
+            className="my-5"
+          >
+            <div style={dropdownMenuStyle}>
+              {countries && countries.length > 0 ? (  // if countries retrieved
+                countries.map((item, index) => (  // display them in dropdpwn
+                  <Dropdown.Item key={index} eventKey={item.country}>
+                    {item.country}
+                  </Dropdown.Item>
+                ))
+              ) : (
+                <Dropdown.Item>Loading countries...</Dropdown.Item>
+              )}
+            </div>
+          </DropdownButton>
 
-      <MapContainer
-        key={`${selectedCountry}-${zoomLevel}`}
-        center={[0, 0]}
-        zoom={zoomLevel}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+          <MapContainer
+            key={`${selectedCountry}-${zoomLevel}`}
+            center={[0, 0]}
+            zoom={zoomLevel}
+            style={{height: "60vh", width: "100%"}}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-        <MarkerClusterGroup>
-          {markers.map(marker =>
-            <Marker position={marker.geocode}>
-              <Popup>
-                <div>
-                  User #{marker.userId} in {marker.city}, {marker.state}<br />
-                  <br />
-                  <a href={marker.link} target="_blank">{marker.title}</a><br />
-                  By {marker.author}<br />
-                  <br />
-                  Score: {marker.score}/10
-                </div>
-              </Popup>
-            </Marker>
-          )}
-        </MarkerClusterGroup>
+            <MarkerClusterGroup>
+              {markers.map(marker =>
+                <Marker position={marker.geocode}>
+                  <Popup>
+                    <div>
+                      User #{marker.userId} in {marker.city}, {marker.state}<br />
+                      <br />
+                      <a href={marker.link} target="_blank">{marker.title}</a><br />
+                      By {marker.author}<br />
+                      <br />
+                      Score: {marker.score}/10
+                    </div>
+                  </Popup>
+                </Marker>
+              )}
+            </MarkerClusterGroup>
 
-      </MapContainer>
-    </Container>
+          </MapContainer>
+        </Container>
+      </div>
+    </>
   );
 };
 
